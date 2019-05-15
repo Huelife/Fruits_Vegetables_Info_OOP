bbb01daf0cbd78c86c38d9666ebf6fdd10f4fcb0
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-15 15:23:17 -0700

    Update Choice.py

--- Choice.py
+++ Choice.py
@@ -3,7 +3,7 @@ import re
 match_fruits = r"fr(uits)*"
 match_vegetables = r"veg(etables)*"
 
-choice = input("Information on 'fruits' or 'vegetables?' ").lower()
+choice = input("Nutrition information on 'fruits' or 'vegetables?' ").lower()
 if re.match(match_fruits, choice):
   import Fruits_OOP.py
 elif re.match(match_vegetables, choice):
