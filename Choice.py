1f22dc93093748854d534f07958dbcff3da6df1f
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-17 20:40:35 -0700

    Update Choice.py

--- Choice.py
+++ Choice.py
@@ -3,14 +3,17 @@ import re
 match_fruits = r"fr(uits)*"
 match_vegetables = r"veg(etables)*"
 
-choice = input("Nutrition information on 'fruits' or 'vegetables?' ").lower()
 while True:
   try:
+    choice = input("Nutrition information on 'fruits' or 'vegetables?' ").lower()
+  except ValueError:
+    continue
+  else:
     if re.match(match_fruits, choice):
       import Fruits_OOP.py
     elif re.match(match_vegetables, choice):
       import Vegetables_OOP.py
     break
-  else:
-    print("Invalid choice.")
-    continue
+    else:
+      print("{} is an invalid choice.".format(choice))
+      continue
