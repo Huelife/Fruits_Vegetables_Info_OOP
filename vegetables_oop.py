0965bd3ec0135bbf666ec182e3666774db686765
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-18 00:48:28 -0700

    Update and rename Vegetables_OOP.py to vegetables_oop.py

--- vegetables_oop.py
+++ vegetables_oop.py
@@ -1,6 +1,7 @@
 #Vegetables super class
 class Vegetables():
   vegetable_name = ""
+  
   def __init__(self,name):
     self.name = name
     
@@ -9,13 +10,16 @@ class Broccoli(Vegetables):
   def __init__(self,name):
     self.vegetable_name = "broccoli"
     super().__init__(name)
+    
   def desc(self):
     if choose_color in ("green","bright green","dark green"):
       print("Green broccoli\n  An edible green plant.")
     elif choose_color in ("yellow","discolored"):
       print("Rotting broccoli\n  An expiring plant.")
+      
   def calorie(self):
     print("Calories(in 148g):\n  50") 
+    
   def nutrition(self):
     print("Nutrition(in 148g):\n  Total Fat: 0.5g\n  Sodium: 49mg\n  Potassium: 468mg\n  Total Carbohydrate: 10g\n  Protein: 4.2g")  
     
@@ -24,13 +28,16 @@ class Spinach(Vegetables):
   def __init__(self,name):
     self.vegetable_name = "spinach"
     super().__init__(name)
+    
   def desc(self):
     if choose_color in ("green","bright green"):
       print("Green spinach\n  An edible, leafy green, flowering plant.")
     elif choose_color in ("black","dark","dark green","mushy","discolored"):
       print("Rotting spinach\n  A spoiled/beginning to spoil, plant.")
+      
   def calorie(self):
     print("Calories(in 100g):\n  23.18")
+    
   def nutrition(self):
     print("Nutrition(in 100g):\n  Total Fat: 0.4g\n  Sodium: 79mg\n  Potassium: 558mg\n  Total Carbohydrate: 3.6g\n  Protein: 2.9g")
     
@@ -39,13 +46,16 @@ class Lettuce(Vegetables):
   def __init__(self,name):
     self.vegetable_name = "lettuce"
     super().__init__(name)
+    
   def desc(self):
     if choose_color in ("green","bright green","dark green"):
       print("Green lettuce\n  A leafy green plant.")
     elif choose_color in ("discolored","brown","black"):
       print("Rotting lettuce\n  An expiring leafy plant.")
+      
   def calorie(self):
     print("Calories(in 36g):\n  5")
+    
   def nutrition(self):
     print("Nutrition(in 36g):\n  Total Fat: 0.1g\n  Sodium: 10mg\n  Potassium: 70mg\n  Total Carbohydrate: 1g\n  Protein: 0.5g")
     
@@ -54,13 +64,16 @@ class Onion(Vegetables):
   def __init__(self,name):
     self.vegetable_name = "onion"
     super().__init__(name)
+    
   def desc(self):
     if choose_color in ("yellow","red","white"):
       print("Common onion\n  A bulb plant.")
     elif choose_color in ("discolored","brown","black"):
       print("Rotting onion\n  An expiring bulb plant.")
+      
   def calorie(self):
     print("Calories(in 110g):\n  44")
+    
   def nutrition(self):
     print("Nutrition(in 110g):\n  Total Fat: 0.1g\n  Sodium: 4mg\n  Potassium: 161mg\n  Total Carbohydrate: 10g\n  Protein: 1.2g")
     
@@ -74,8 +87,10 @@ vegetable_dict = {
 
 #user input and printing information
 choose_color = input("What color is the vegetable? ").lower()
+
 for name,value in vegetable_dict.items():
   print(" "+name)
+  
 choose_vegetable = input("Which vegetable would you like information on? ").lower()
 if choose_vegetable in vegetable_dict:
   vegetable_dict[choose_vegetable]("").desc()
