df61831102e60f4a7708b171112bc66ebd00d350
Eric Meas <49707840+Huelife@users.noreply.github.com>
2019-05-22 01:06:58 -0700

    Update choice.py
    added comments and also wrapped long lines

--- choice.py
+++ choice.py
@@ -1,16 +1,19 @@
 import re
 
+#user input matchs fr+ and veg+
 match_fruits = r"fr(uits)*"
 match_vegetables = r"veg(etables)*"
 
+#user can choose between fruits or vegetables, but repeats if neither
 while True:
   try:
-    choice = input("Nutrition information on 'fruits' or 'vegetables?' ").lower()
+    choice = (input("Nutrition information on 'fruits' or 'vegetables?' ")
+              .lower())
   except ValueError:
     continue
   else:
     if re.match(match_fruits, choice):
-      import fruits_oop.py
+      import fruits_oop.py    
     elif re.match(match_vegetables, choice):
       import vegetables_oop.py
     break
